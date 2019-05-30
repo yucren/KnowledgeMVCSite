9d639e3145b8cc53db77433a339054425be93134
chengren yu <yu8611263@live.cn>
2019-05-30 23:08:50 +0800

    添加账户管理

--- KnowledgeMVCSite/Controllers/ManageController.cs
+++ KnowledgeMVCSite/Controllers/ManageController.cs
@@ -1,15 +1,19 @@
 ﻿using KnowledgeMVCSite.App_Start;
+using KnowledgeMVCSite.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace KnowledgeMVCSite.Controllers
 {
+    [Authorize]
     public class ManageController : Controller
     {
         private ApplicationSignInManager _signInManager;
@@ -48,6 +52,13 @@ namespace KnowledgeMVCSite.Controllers
                 _userManager = value;
             }
         }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
 
         // GET: Manager
         public async Task<ActionResult> Index(ManageMessageId? message)
@@ -59,15 +70,64 @@ namespace KnowledgeMVCSite.Controllers
             : message == ManageMessageId.RemoveLoginSuccess ? "已删除你的电话号码。"
             : "";
             var userId = User.Identity.GetUserId();
-            var model=new indexviewmodel
+            var model = new IndexViewModel
             {
+                HasPassword = HasPassword(),
+                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
+                Logins = await UserManager.GetLoginsAsync(userId),
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
 
-            }
-
+            };
+            return View(model);
+        }
+        [Authorize(Roles = "Administrators")]
+        public ActionResult ChangePassword()
+        {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result == IdentityResult.Success)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+                }
+               
 
+            }
+
+            AddErrors(result);
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        public bool HasPassword()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            if (user !=null)
+            {
+                return user.PasswordHash != null;
+            }
+            return false;   
+        }
         public enum ManageMessageId
         {
             AddPhoneSuccess,
--- KnowledgeMVCSite/Models/ManageViewModels.cs
+++ KnowledgeMVCSite/Models/ManageViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,19 @@ namespace KnowledgeMVCSite.Models
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
         public bool BrowserRemembered { get; set; }
+    }
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="新密码")]
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name ="确实新密码")]
+        [Compare("NewPassword", ErrorMessage = "新密码和确认密码不匹配。"   )]
+        public string ConfirmPassword { get; set; }
+
     }
 
 }
