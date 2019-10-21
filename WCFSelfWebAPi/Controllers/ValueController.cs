a7034db53e768c0dedbaacf21eabdd765097af88
chengren yu <yu8611263@live.cn>
2019-10-21 22:09:59 +0800

    1

---
+++ WCFSelfWebAPi/Controllers/ValueController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace WCFSelfWebAPi.Controllers
+{
+    public class ValueController : ApiController
+    {
+       public string Get(int id)
+        {
+            return "values" + id;
+        }
+
+    }
+}
---
+++ WCFSelfWebAPi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WCFSelfWebAPi
+{
+    [SecuritySafeCritical]
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8089/");
+            config.Routes.MapHttpRoute(
+                name:"DefaultApi",
+                routeTemplate:"api/{controller}/{id}",
+                defaults:new {id=RouteParameter.Optional}
+                );
+            var server = new HttpSelfHostServer(config);
+            server.OpenAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
