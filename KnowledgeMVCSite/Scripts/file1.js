6ae520f5ef6191d6f9497d05ea6ade840073dfcb
yuchengren <539928505@qq.com>
2020-03-19 21:30:43 +0800

    11

---
+++ KnowledgeMVCSite/Scripts/file1.js
@@ -0,0 +1,12 @@
+var User = /** @class */ (function () {
+    function User(name, email, createTime) {
+        this.name = name;
+        this.email = email;
+        this.createTime = createTime;
+        this.name = name;
+        this.email = email;
+        this.createTime = createTime;
+    }
+    return User;
+}());
+//# sourceMappingURL=file1.js.map
---
+++ KnowledgeMVCSite/Scripts/file1.ts
@@ -0,0 +1,7 @@
+﻿class User {
+    constructor(public name: string, public email: string, public createTime: Date) {
+        this.name = name;
+        this.email = email;
+        this.createTime = createTime;
+    }
+}
